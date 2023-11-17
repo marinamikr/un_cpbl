117101f142a1138b97077b5ff5a3e40a09cb8223
原田摩利奈 <marina@maMacBook-Pro-2.local>
2023-11-17 19:45:00 +0900

    f

---
+++ uni_cpblUITests/uni_cpblUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  uni_cpblUITestsLaunchTests.swift
+//  uni_cpblUITests
+//
+//  Created by 原田摩利奈 on 2023/11/17.
+//
+
+import XCTest
+
+final class uni_cpblUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
