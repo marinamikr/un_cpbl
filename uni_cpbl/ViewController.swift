83d24b67169b561eb85d49ef8a306434d397085d
原田摩利奈 <marina@maMacBook-Pro-2.local>
2023-11-17 20:56:34 +0900

    map

--- uni_cpbl/ViewController.swift
+++ uni_cpbl/ViewController.swift
@@ -6,12 +6,32 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    var locationManager: CLLocationManager! // locationManagerの宣言
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     super.viewDidLoad()
+
+     locationManager = CLLocationManager()
+     locationManager.delegate = self
+     locationManager!.requestWhenInUseAuthorization()
+    }
+
+    func locationManager(_ manager: CLLocationManager,didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined: // 許可されてない場合
+            manager.requestWhenInUseAuthorization() // 許可を求める（ダイアログが表示される）
+        case .restricted, .denied: // 拒否されてる場合
+            break
+        case .authorizedAlways, .authorizedWhenInUse: // 許可されている場合
+            manager.startUpdatingLocation() // 現在地の取得を開始
+            break
+        default:
+            break
+        }
     }
 
 
